fd7c6886d65c035c0598600325d84b1e597116db
Muhammad Ahmad Chand <ahmadchand420@gmail.com>
2020-10-26 04:12:50 -0700

    Update and rename Two Strings.cs to Sherlock And Anagrams.cs

--- Interview Preparation Kit/Sherlock And Anagrams.cs
+++ Interview Preparation Kit/Sherlock And Anagrams.cs
@@ -5,7 +5,6 @@ using System.Linq;
 
 class Solution
 {
-
     // Complete the sherlockAndAnagrams function below.
     static int sherlockAndAnagrams(string s)
     {
@@ -36,19 +35,15 @@ class Solution
     static void Main(string[] args)
     {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
         int q = Convert.ToInt32(Console.ReadLine());
 
         for (int qItr = 0; qItr < q; qItr++)
         {
             string s = Console.ReadLine();
-
             int result = sherlockAndAnagrams(s);
-
             textWriter.WriteLine(result);
         }
         textWriter.Flush();
         textWriter.Close();
-
     }
 }
